6c021f0ff0426404d8522c04a41887ec965b0272
Patrick Tavares <tavarespatrick01@gmail.com>
2023-01-27 01:13:51 +0100

    :tada: initial commit

---
+++ outputs.tf
@@ -0,0 +1,6 @@
+################################################################################
+# Outputs
+################################################################################
+
+# All your module outputs that can be used by another one or just for info
+# Use "sensitive = true" if you don't want the value be print in logs
